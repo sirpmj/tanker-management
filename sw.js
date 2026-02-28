37a2629ec7d6f9e739d1a9506f260d75ce97efc6
sirpmj <SIRPMJ@GMAIL.COM>
2026-02-28 13:41:42 +0530

    Upgrade service worker to version 2 with enhancements
    Updated service worker to version 2 with new caching strategy and added icon files.

--- sw.js
+++ sw.js
@@ -1,22 +1,43 @@
-const CACHE_NAME = "tanker-app-v1";
+const CACHE_NAME = "tanker-app-v2";
+
 const urlsToCache = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
+// INSTALL
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// ACTIVATE
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// FETCH
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match("./index.html"))
   );
 });
